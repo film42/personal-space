bf2d76c62bef3baa674650e9b6a20a80dddd9da5
Garrett Thornburg <film42@gmail.com>
2017-08-23 20:55:59 -0400

    Start trying to compress

--- stream.go
+++ stream.go
@@ -2,11 +2,55 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
 	"io"
 )
+
+type NoOpWriteCloser struct {
+	*io.Writer
+}
+func (c *NoOpWriteCloser) Write(p []byte) (int, error) {
+	return c.Writer.Write(p)
+}
+func (c *NoOpWriteCloser) Close() error {
+	return nil
+}
+
+type Redirect struct {
+	src io.Reader
+	srcEOF bool
+	redirect io.WriteCloser
+	dest io.Reader
+}
+func NewRedirect(src io.Reader, redirect io.WriteCloser, dest io.Reader) *Redirect {
+	var writeCloser io.WriteCloser
+	if wcloser, ok := redirect.(io.WriteCloser); ok {
+		writeCloser =  wcloser
+	} else {
+		writeCloser = &NoOpWriteCloser{redirect}
+	}
+
+	return &Redirect{
+		src: io.TeeReader(src, redirect),
+		srcEOF: false,
+		dest: dest,
+		redirect: writeCloser,
+	}
+}
+func (r *Redirect) Read(p []byte) (int, error) {
+	if !r.srcEOF {
+		_, err := r.src.Read(p)
+		if err == io.EOF {
+			r.srcEOF = true
+			r.redirect.Close()
+		}
+	}
+	return r.dest.Read(p)
+}
+
 
 type Stream struct {
 	block cipher.Block
@@ -21,10 +65,9 @@ func NewStream(key string) (*Stream, error) {
 	return &Stream{block: block}, nil
 }
 
-func (s *Stream) newStream(src io.Reader, iv []byte) io.Reader {
-	// Wrap the body in a streaming OFB encryption block
-	stream := cipher.NewOFB(s.block, iv[:])
-	return &cipher.StreamReader{S: stream, R: src}
+func Read(b []byte) (int, error) {
+	stream.Input.ReadUpTo(len(b))
+	return stream.Output.Read(b)
 }
 
 func (s *Stream) EncryptReader(srcReader io.Reader) (io.Reader, error) {
@@ -33,10 +76,13 @@ func (s *Stream) EncryptReader(srcReader io.Reader) (io.Reader, error) {
 	if _, err := io.ReadFull(rand.Reader, iv[:]); err != nil {
 		return nil, err
 	}
-
 	ivReader := bytes.NewReader(iv[:])
-	encryptedSrc := s.newStream(srcReader, iv[:])
-	ivAndEncryptedSrcReader := io.MultiReader(ivReader, encryptedSrc)
+
+	bodyBuffer := &bytes.Buffer{}
+	encryptWriter := &cipher.StreamWriter{S: stream, W: bodyBuffer}
+	compressWriter := gzip.NewWriter(encryptWriter)
+
+	ivAndEncryptedSrcReader := io.MultiReader(ivReader, bodyBuffer)
 	return ivAndEncryptedSrcReader, nil
 }
 
@@ -45,5 +91,10 @@ func (s *Stream) DecryptReader(src io.Reader) (io.Reader, error) {
 	if _, err := io.ReadFull(src, iv[:]); err != nil {
 		return nil, err
 	}
+
+	// Wrap the body in a streaming OFB encryption block
+	stream := cipher.NewOFB(s.block, iv[:])
+	reader := &cipher.StreamReader{S: stream, R: src}
+
 	return s.newStream(src, iv[:]), nil
 }
